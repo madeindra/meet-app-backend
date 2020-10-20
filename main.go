8f83795cc0e9c3c35d0ca2706452a632421d5041
Made Indra <made.indra@protonmail.com>
2020-10-20 11:33:17 +0700

    first working server

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"success": true, "message": "Server is working properly"})
+	})
+
+	router.Run(":8080")
+}
