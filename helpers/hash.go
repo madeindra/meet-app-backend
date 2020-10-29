4c55f7c635291f8fc93d6393b553a883c9c64224
Made Indra <made.indra@protonmail.com>
2020-10-29 16:39:28 +0700

    add hash helpers

---
+++ helpers/hash.go
@@ -0,0 +1,27 @@
+package helpers
+
+import (
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func GenerateHash(password string) (string, error) {
+	bytePassword := []byte(password)
+	byteHash, err := bcrypt.GenerateFromPassword(bytePassword, bcrypt.MinCost)
+	if err != nil {
+		return "", errors.New("Hash failed")
+	}
+
+	hashed := string(byteHash)
+	return hashed, nil
+}
+
+func VerifyHash(hashedPassword string, plainPassword string) error {
+	byteHash := []byte(hashedPassword)
+	bytePassword := []byte(plainPassword)
+
+	err := bcrypt.CompareHashAndPassword(byteHash, bytePassword)
+
+	return err
+}
