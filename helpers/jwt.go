a6421a43a4ddb1e15838fe0ac33019e18c040b65
Made Indra <made.indra@protonmail.com>
2020-10-30 18:00:25 +0700

    remove uneeded jwt helper

--- helpers/jwt.go
+++ helpers/jwt.go
@@ -1,8 +1,6 @@
 package helpers
 
 import (
-	"errors"
-	"fmt"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
@@ -27,29 +25,4 @@ func CreateJWT(email string) (string, error) {
 	}
 
 	return tokenString, nil
-}
-
-func ValidateJWT(token string) (*jwt.Token, error) {
-	validated, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-		}
-
-		return []byte(signingKey), nil
-	})
-
-	return validated, err
-}
-
-func ParseJWT(token string) (jwt.MapClaims, error) {
-	validated, err := ValidateJWT(token)
-	if err != nil {
-		return nil, err
-	}
-
-	if claims, ok := validated.Claims.(jwt.MapClaims); ok && validated.Valid {
-		return claims, nil
-	}
-
-	return nil, errors.New("Unable to parse token")
 }
