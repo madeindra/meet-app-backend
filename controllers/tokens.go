c91083167e6ae6363d429d729a696aa3ad2c7c1b
Made Indra <made.indra@protonmail.com>
2020-10-31 21:14:29 +0700

    update token controller

--- controllers/tokens.go
+++ controllers/tokens.go
@@ -2,41 +2,75 @@ package controllers
 
 import (
 	"net/http"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/madeindra/meet-app/helpers"
 	"github.com/madeindra/meet-app/models"
 	"github.com/madeindra/meet-app/responses"
 )
 
 type TokenController struct {
-	token models.TokenInterface
+	token      models.TokenInterface
+	credential models.CredentialInterface
 }
 
-func NewTokenController(token models.TokenInterface) *TokenController {
-	return &TokenController{token}
+func NewTokenController(token models.TokenInterface, credential models.CredentialInterface) *TokenController {
+	return &TokenController{token, credential}
 }
 
-func (controller *TokenController) GetSingle(ctx *gin.Context) {
-	param := ctx.Param("id")
+func (controller *TokenController) Refresh(ctx *gin.Context) {
+	data := models.NewTokenData(0, "")
+	if err := ctx.ShouldBindJSON(&data); err != nil {
+		res := responses.BadRequestResponse()
+		ctx.JSON(http.StatusBadRequest, res)
+		return
+	}
 
-	id, err := strconv.ParseUint(param, 10, 64)
+	email, err := helpers.ParseRefreshToken(data.RefreshToken)
 	if err != nil {
 		res := responses.BadRequestResponse()
 		ctx.JSON(http.StatusBadRequest, res)
 		return
 	}
 
-	data := models.NewTokenData(id, "")
+	user := models.NewCredentialData(email, "")
+	userData := controller.credential.FindOne(user)
+	if userData.ID == 0 {
+		res := responses.NotFoundResponse()
+		ctx.JSON(http.StatusNotFound, res)
+		return
+	}
 
-	token := controller.token.FindOne(data)
+	tokenData := models.NewTokenData(userData.ID, data.RefreshToken)
+	token := controller.token.FindOne(tokenData)
 	if token.ID == 0 {
 		res := responses.NotFoundResponse()
 		ctx.JSON(http.StatusNotFound, res)
 		return
 	}
 
-	res := responses.NewTokenResponse(token.UserID, token.RefreshToken)
+	authToken, err := helpers.CreateJWT(userData.Email)
+	if err != nil {
+		res := responses.InterenalServerErrorResponse()
+		ctx.JSON(http.StatusInternalServerError, res)
+		return
+	}
+
+	refreshToken, err := helpers.CreateRefreshToken(userData.Email)
+	if err != nil {
+		res := responses.InterenalServerErrorResponse()
+		ctx.JSON(http.StatusInternalServerError, res)
+		return
+	}
+
+	token.RefreshToken = refreshToken
+	if _, err := controller.token.Update(token); err != nil {
+		res := responses.InterenalServerErrorResponse()
+		ctx.JSON(http.StatusInternalServerError, res)
+		return
+	}
+
+	res := responses.NewTokenResponse(authToken, token.RefreshToken)
 	ctx.JSON(http.StatusOK, res)
 	return
 }
