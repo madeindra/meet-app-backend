518a9081af7d3f2cef32c43c405ce91ef322c1b2
Made Indra <made.indra@protonmail.com>
2020-11-03 17:13:51 +0700

    update profile schema

--- models/profiles.go
+++ models/profiles.go
@@ -3,14 +3,17 @@ package models
 import "github.com/jinzhu/gorm"
 
 type profiles struct {
-	ID        uint64 `json:"id" gorm:"primaryKey"`
-	UserID    uint64 `json:"userId"`
-	FirstName string `json:"firstName" binding:"required"`
-	LastName  string `json:"lastName" binding:"required"`
+	ID          uint64  `json:"id" gorm:"primaryKey"`
+	UserID      uint64  `json:"userId"`
+	FirstName   string  `json:"firstName" binding:"required"`
+	LastName    string  `json:"lastName" binding:"required"`
+	Description string  `json:"description"`
+	Latitude    float64 `json:"latitude"`
+	Longitude   float64 `json:"longitude"`
 }
 
 type ProfilesInterface interface {
-	New(userId uint64, firstName string, lastName string) profiles
+	New(userId uint64, firstName string, lastName string, description string, latitude float64, longitude float64) profiles
 	Create(data profiles) (profiles, error)
 	FindOne(data profiles) profiles
 }
@@ -23,8 +26,8 @@ func NewProfileModel(db *gorm.DB) *ProfilesImplementation {
 	return &ProfilesImplementation{db}
 }
 
-func (implementation *ProfilesImplementation) New(userId uint64, firstName string, lastName string) profiles {
-	return profiles{UserID: userId, FirstName: firstName, LastName: lastName}
+func (implementation *ProfilesImplementation) New(userId uint64, firstName string, lastName string, description string, latitude float64, longitude float64) profiles {
+	return profiles{UserID: userId, FirstName: firstName, LastName: lastName, Description: description, Latitude: latitude, Longitude: longitude}
 }
 
 func (implementation *ProfilesImplementation) Create(data profiles) (profiles, error) {
