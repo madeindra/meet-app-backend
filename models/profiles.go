000280dc1e4da14424dc0c809fbb1e1f1e1ec3ba
Made Indra <made.indra@protonmail.com>
2020-11-04 19:54:15 +0700

    fix profiles model

--- models/profiles.go
+++ models/profiles.go
@@ -1,10 +1,14 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"errors"
+
+	"github.com/jinzhu/gorm"
+)
 
 type profiles struct {
 	ID          uint64  `json:"id" gorm:"primaryKey"`
-	UserID      uint64  `json:"userId"`
+	UserID      uint64  `json:"userId" binding:"required"`
 	FirstName   string  `json:"firstName" binding:"required"`
 	LastName    string  `json:"lastName" binding:"required"`
 	Description string  `json:"description"`
@@ -15,7 +19,10 @@ type profiles struct {
 type ProfilesInterface interface {
 	New(userId uint64, firstName string, lastName string, description string, latitude float64, longitude float64) profiles
 	Create(data profiles) (profiles, error)
-	FindOne(data profiles) profiles
+	FindAll() []profiles
+	FindByUser(data profiles) profiles
+	UpdateByUser(data profiles) (profiles, error)
+	DeleteByUser(data profiles) error
 }
 
 type ProfilesImplementation struct {
@@ -41,11 +48,45 @@ func (implementation *ProfilesImplementation) Create(data profiles) (profiles, e
 	return data, tx.Commit().Error
 }
 
-func (implementation *ProfilesImplementation) FindOne(data profiles) profiles {
-	tx := implementation.db
-	res := profiles{}
+func (implementation *ProfilesImplementation) FindAll() []profiles {
+	res := []profiles{}
 
-	tx.Where(data).First(&res)
+	implementation.db.Find(&res)
 
 	return res
+}
+
+func (implementation *ProfilesImplementation) FindByUser(data profiles) profiles {
+	res := profiles{}
+
+	implementation.db.Where(data).First(&res)
+
+	return res
+}
+
+func (implementation *ProfilesImplementation) UpdateByUser(data profiles) (profiles, error) {
+	tx := implementation.db.Begin()
+	res := profiles{}
+
+	if err := tx.First(&res).Updates(data).Error; err != nil {
+		tx.Rollback()
+		return profiles{}, err
+	}
+
+	if res.ID == 0 {
+		return profiles{}, errors.New("Not found")
+	}
+
+	return res, tx.Commit().Error
+}
+
+func (implementation *ProfilesImplementation) DeleteByUser(data profiles) error {
+	tx := implementation.db.Begin()
+
+	if err := tx.Where(data).Delete(data).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return tx.Commit().Error
 }
