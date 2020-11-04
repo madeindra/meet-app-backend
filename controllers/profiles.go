88eecc337191c7654ea7605eef842ee671195cc1
Made Indra <made.indra@protonmail.com>
2020-11-04 19:54:22 +0700

    WIP Profile controller

--- controllers/profiles.go
+++ controllers/profiles.go
@@ -1,8 +1,12 @@
 package controllers
 
 import (
+	"net/http"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 	"github.com/madeindra/meet-app/models"
+	"github.com/madeindra/meet-app/responses"
 )
 
 type ProfilesController struct {
@@ -14,21 +18,59 @@ func NewProfileController(profile models.ProfilesInterface) *ProfilesController 
 }
 
 func (controller *ProfilesController) GetSingle(ctx *gin.Context) {
+	id, _ := strconv.ParseUint(ctx.Param("id"), 10, 64)
+	data := controller.profile.New(id, "", "", "", 0, 0)
+
+	res := controller.profile.FindByUser(data)
+	ctx.JSON(http.StatusOK, res)
 	return
 }
 
 func (controller *ProfilesController) GetCollections(ctx *gin.Context) {
+	res := controller.profile.FindAll()
+	ctx.JSON(http.StatusOK, res)
 	return
 }
 
 func (controller *ProfilesController) Post(ctx *gin.Context) {
+	data := controller.profile.New(0, "", "", "", 0, 0)
+	if err := ctx.ShouldBindJSON(&data); err != nil {
+		res := responses.BadRequestResponse()
+		ctx.JSON(http.StatusBadRequest, res)
+		return
+	}
+
+	res, err := controller.profile.Create(data)
+	if err != nil {
+		res := responses.InterenalServerErrorResponse()
+		ctx.JSON(http.StatusInternalServerError, res)
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, res)
 	return
 }
 
 func (controller *ProfilesController) Put(ctx *gin.Context) {
+	id, _ := strconv.ParseUint(ctx.Param("id"), 10, 64)
+
+	data := controller.profile.New(id, "", "", "", 0, 0)
+	if err := ctx.ShouldBindJSON(&data); err != nil {
+		res := responses.BadRequestResponse()
+		ctx.JSON(http.StatusBadRequest, res)
+		return
+	}
+
+	res, _ := controller.profile.UpdateByUser(data)
+	ctx.JSON(http.StatusOK, res)
 	return
 }
 
 func (controller *ProfilesController) Delete(ctx *gin.Context) {
+	id, _ := strconv.ParseUint(ctx.Param("id"), 10, 64)
+	data := controller.profile.New(id, "", "", "", 0, 0)
+
+	res := controller.profile.DeleteByUser(data)
+	ctx.JSON(http.StatusNoContent, res)
 	return
 }
