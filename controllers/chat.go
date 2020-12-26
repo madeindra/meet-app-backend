cb379541858fbd8fb5de2f1c88c8408d21538cbf
Made Indra <made.indra@protonmail.com>
2020-12-26 21:37:19 +0700

    create controller to fetch latest chat

--- controllers/chat.go
+++ controllers/chat.go
@@ -20,10 +20,6 @@ func NewChatController(chat models.ChatsInterface, credential models.CredentialI
 }
 
 func (controller *ChatController) GetDetail(ctx *gin.Context) {
-	// TODO: implement latest chat only
-	// get latest query from url
-	// latestOnly := ctx.DefaultQuery("latest", "false")
-
 	// get target ID from param
 	id, err := strconv.ParseUint(ctx.Param("id"), 10, 64)
 	if err != nil {
@@ -51,15 +47,49 @@ func (controller *ChatController) GetDetail(ctx *gin.Context) {
 		return
 	}
 
-	// find chat where above user is the sender and id param is the target
+	// find chat where above user is the sender
 	chat := controller.chat.New()
 	chat.Sender = user.ID
-	chat.Target = id
 
+	// also find where id in param is the target
+	chat.Target = id
 	details := controller.chat.FindBy(chat)
 
 	// return without checking data length
 	res := entities.NewChatBatchResponse(details)
 	ctx.JSON(http.StatusOK, res)
 	return
+}
+
+func (controller *ChatController) GetLatest(ctx *gin.Context) {
+	// get email from jwt
+	email, set := ctx.Get("sub")
+	if !set {
+		res := entities.InterenalServerErrorResponse()
+		ctx.JSON(http.StatusInternalServerError, res)
+		return
+	}
+
+	// find user by id and email in database
+	data := controller.credential.New()
+	data.Email = fmt.Sprintf("%v", email)
+
+	user := controller.credential.FindOne(data)
+	if user.ID == 0 {
+		res := entities.NotFoundResponse()
+		ctx.JSON(http.StatusNotFound, res)
+		return
+	}
+
+	// find latest chat where above user is the sender
+	chat := controller.chat.New()
+	chat.Sender = user.ID
+
+	details := controller.chat.FindDistinct(chat)
+
+	// return without checking data length
+	res := entities.NewChatBatchResponse(details)
+	ctx.JSON(http.StatusOK, res)
+	return
+
 }
