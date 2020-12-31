0724a2f92d6ca04da256c3b7147804d6e5a295cc
Made Indra <made.indra@protonmail.com>
2020-12-31 21:48:54 +0700

    add senderName & senderPicture to chat

--- entities/chat.go
+++ entities/chat.go
@@ -13,21 +13,23 @@ type chatBatchResponse struct {
 }
 
 type chatResponseData struct {
-	ID      uint64 `gorm:"primaryKey" json:"id,omitempty"`
-	Sender  uint64 `json:"senderId"`
-	Target  uint64 `json:"targetId"`
-	Content string `json:"content"`
+	ID            uint64 `gorm:"primaryKey" json:"id,omitempty"`
+	SenderID      uint64 `json:"senderId"`
+	SenderName    string `json:"senderName"`
+	SenderPicture string `json:"senderPicture"`
+	TargetID      uint64 `json:"targetId"`
+	Content       string `json:"content"`
 }
 
 func NewChatResponse(id uint64, senderID uint64, targetID uint64, content string) chatResponse {
-	data := chatResponseData{ID: id, Sender: senderID, Target: targetID, Content: content}
+	data := chatResponseData{ID: id, SenderID: senderID, TargetID: targetID, Content: content}
 	return chatResponse{Status: true, Data: data}
 }
 
 func NewChatBatchResponse(chats []models.Chats) chatBatchResponse {
 	data := []chatResponseData{}
 	for i := range chats {
-		data = append(data, chatResponseData{ID: chats[i].ID, Sender: chats[i].SenderID, Target: chats[i].TargetID, Content: chats[i].Content})
+		data = append(data, chatResponseData{ID: chats[i].ID, SenderID: chats[i].SenderID, SenderName: chats[i].SenderName, SenderPicture: chats[i].SenderPicture, TargetID: chats[i].TargetID, Content: chats[i].Content})
 	}
 	return chatBatchResponse{Status: true, Message: operationSuccessMessage, Data: data}
 }
--- models/chat.go
+++ models/chat.go
@@ -5,10 +5,12 @@ import (
 )
 
 type Chats struct {
-	ID       uint64 `gorm:"primaryKey" json:",omitempty"`
-	SenderID uint64
-	TargetID uint64
-	Content  string
+	ID            uint64 `gorm:"primaryKey" json:",omitempty"`
+	SenderID      uint64
+	TargetID      uint64
+	Content       string
+	SenderName    string
+	SenderPicture string
 }
 
 type ChatsInterface interface {
@@ -52,7 +54,7 @@ func (implementation *ChatsImplementation) FindBy(data Chats) []Chats {
 func (implementation *ChatsImplementation) FindDistinct(data Chats) []Chats {
 	res := []Chats{}
 
-	implementation.db.Raw("SELECT t1.* FROM chats AS t1 INNER JOIN (SELECT MIN(sender_id, target_id) AS sender_id, MAX(sender_id, target_id) AS target_id, MAX(id) AS max_id FROM chats GROUP BY MIN(sender_id, target_id), MAX(sender_id, target_id)) AS t2 ON MIN(t1.sender_id, t1.target_id) = t2.sender_id AND MAX(t1.sender_id, t1.target_id) = t2.target_id AND t1.id = t2.max_id WHERE t1.sender_id = ? OR t1.target_id = ?", data.SenderID, data.SenderID).Scan(&res)
+	implementation.db.Raw("SELECT t3.*, t4.name as sender_name, t4.picture as sender_picture FROM(SELECT t1.* FROM chats AS t1 INNER JOIN (SELECT MIN(sender_id, target_id) AS sender_id, MAX(sender_id, target_id) AS target_id, MAX(id) AS max_id FROM chats GROUP BY MIN(sender_id, target_id), MAX(sender_id, target_id)) AS t2 ON MIN(t1.sender_id, t1.target_id) = t2.sender_id AND MAX(t1.sender_id, t1.target_id) = t2.target_id AND t1.id = t2.max_id WHERE t1.sender_id = ? OR t1.target_id = ?) as t3 INNER JOIN profiles as t4 WHERE t3.sender_id = t4.id", data.SenderID, data.SenderID).Scan(&res)
 
 	return res
 }
